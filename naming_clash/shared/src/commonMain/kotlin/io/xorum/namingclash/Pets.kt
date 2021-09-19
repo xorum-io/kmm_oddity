3967521c98f9861f6f553580c861dc9c7eb74b66
Yev Kanivets <yevhenii.kanivets@gmail.com>
2021-09-19 18:13:24 +0300

    Extract DogFood and CatFood.

--- naming_clash/shared/src/commonMain/kotlin/io/xorum/namingclash/Pets.kt
+++ naming_clash/shared/src/commonMain/kotlin/io/xorum/namingclash/Pets.kt
@@ -2,14 +2,14 @@ package io.xorum.namingclash
 
 interface Dog {
 
-    fun eat(food: Food)
-
-    class Food
+    fun eat(food: DogFood)
 }
+
+class DogFood
 
 interface Cat {
 
-    fun eat(food: Food)
-
-    class Food
+    fun eat(food: CatFood)
 }
+
+class CatFood
